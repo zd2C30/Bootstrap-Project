63e2f6e64a3b1f429bf0b8f6df3a9c30e2e6b996
Kaori Yoshida <zd2C30@gmail.com>
2017-12-21 13:32:23 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict'
+
+var gulp = require("gulp");
+var del = require("del");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
+var minifycss = require('gulp-minify-css');
+
+gulp.task("delHtml", function() {
+  del(["html/*.html"]);
+});
+gulp.task("delSass", function() {
+  del(["html/css/*.css"]);
+});
+
+gulp.task("copy", function() {
+  return gulp.src(["assets/**/*.html"])
+  .pipe(gulp.dest("html/"))
+  .pipe(browserSync.stream());
+});
+
+gulp.task("sass", function() {
+  return gulp.src(["assets/stylesheets/**/*.scss"])
+  .pipe(sass({outputStyle: 'compressed'}))
+  .pipe(gulp.dest("html/css/"))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['delHtml','delSass','copy','sass'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "html"
+    }
+  });
+  gulp.watch(['assets/**/*.html'], ['delHtml','copy']);
+  gulp.watch(['assets/stylesheets/**/*.scss'], ['delSass','sass']);
+});
